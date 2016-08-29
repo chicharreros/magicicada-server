34ab41287a6f97bce6ed748ea9a5ff3b51b5489e
magicicada <nataliabidart+magicicada@gmail.com>
2016-08-29 18:00:22 -0300

    72: Natalia 2016-08-29 [merge] [r=nataliabidart] - Ignore flake8 for testing settings file.

--- magicicada/settings/testing.py
+++ magicicada/settings/testing.py
@@ -1,4 +1,6 @@
-from magicicada.settings import *  # noqa
+# flake8: noqa
+
+from magicicada.settings import *
 
 LOGGING['handlers']['server']['filename'] = os.path.join(
     LOG_FOLDER, 'filesync-server-tests.log')
